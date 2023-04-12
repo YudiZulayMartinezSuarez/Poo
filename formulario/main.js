215be81d695c49457e807e303c354db22e9fd047
YudiZulayMartinezSuarez <124052828+YudiZulayMartinezSuarez@users.noreply.github.com>
2023-04-12 13:33:41 -0500

    Add files via upload

--- formulario/main.js
+++ formulario/main.js
@@ -1,7 +1,7 @@
 
 class lapiz{
     #marca
-    constructor({color="fff700",
+    constructor({color="#F0D000",
                 dimension=19,
                 borrador=true,
                 material="Madera",
@@ -16,18 +16,38 @@ class lapiz{
     return this.color;
     }
 
-    getDimencion(){
-        return this.Dimencion;
+    getDimension(){
+        return this.Dimension;
+    }
+
+    getMarca(){
+        return this.Marca;
+    }
+
+    getBorrador(){
+        return this.borrador;
+    }
+    getMaterial(){
+        return this.material;
     }
 }
 let obj = undefined
-let color = document.querySelector(`[name= "colo"]`)
-
-let dimension =
+let color = document.querySelector(`[name= "color"]`)
+let dimension = document.querySelector(`[name = "dimension"]`)
+//let marca = document.querySelector(`[name="marca"]`)
+let borrador = tiene.checked=true;
+let material = madera.checked=true;
 addEventListener("DOMContentLoaded",(e)=>{
     obj = new lapiz ({});
     color.value = obj.color;
+    dimension.value = obj.dimension;
+    //marca.value=obj.marca;
+    borrador.getElementById(tiene)= obj.borrador;
+    material.getElementById(marca)=obj.material;
+
+
 })
+
 
 
 
